0eca81906d42d208b07da90b9b68b2706916d2e2
andresha95 <uncle.psu@gmail.com>
2016-06-15 06:40:03 -0700

    new TaskListProject

---
+++ src/ApiBundle/Entity/User.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Uncle
+ * Date: 10.06.2016
+ * Time: 23:54
+ */
+
+namespace ApiBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table("users")
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
